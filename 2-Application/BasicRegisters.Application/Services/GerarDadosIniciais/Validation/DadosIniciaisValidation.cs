21cd80fa0c716325cff810e2ac2a20527b81a1d9
brunoofgod <brunoofgod@gmail.com>
2018-08-22 08:35:23 -0300

    Implementado validator dos dados iniciais

--- 2-Application/BasicRegisters.Application/Services/GerarDadosIniciais/Validation/DadosIniciaisValidation.cs
+++ 2-Application/BasicRegisters.Application/Services/GerarDadosIniciais/Validation/DadosIniciaisValidation.cs
@@ -8,8 +8,32 @@ namespace BasicRegisters.Application.Services.GerarDadosIniciais.Validation
         public DadosIniciaisValidation()
         {
             RuleFor(c => c.ApelidoDaConta)
-                .NotNull()
-                .WithMessage("Não pode ser nullo");
+                .MinimumLength(3)
+                .WithMessage("O apelido da conta deve ter no minimo 3 caracteres.")
+                .MaximumLength(20)
+                .WithMessage("O apelido da conta não pode ter mais que 20 caracteres.");
+
+            RuleFor(c => c.ApelidoParaUsuarioAdministrador)
+                .MinimumLength(3)
+                .WithMessage("O apelido do usuário deve ter no minimo 3 caracteres.")
+                .MaximumLength(32)
+                .WithMessage("O apelido do usuário não pode ter mais que 20 caracteres.");
+
+            RuleFor(c => c.EmailParaUsuarioAdministrador)
+                .MinimumLength(3)
+                .WithMessage("O e-mail do usuário deve ter no minimo 3 caracteres.")
+                .MaximumLength(120)
+                .WithMessage("O e-mail do usuário não pode ter mais que 120 caracteres.");
+
+            RuleFor(c => c.NomeDaConta)
+                .MinimumLength(3)
+                .WithMessage("O e-mail do usuário deve ter no minimo 3 caracteres.")
+                .MaximumLength(120)
+                .WithMessage("O e-mail do usuário não pode ter mais que 120 caracteres.");
+
+            RuleFor(c => c.SenhaParaUsuarioAdministrador)
+                .NotEmpty()
+                .WithMessage("A senha do usuário administrador deve ser preenchida.");
         }
     }
 }
